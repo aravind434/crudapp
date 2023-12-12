dca237c30ec2bf0f3ca8884a66b6917cf823213b
aravind434 <aravindmekala434@gmail.com>
2023-12-12 11:31:00 +0530

    base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingComponent } from './landing/landing.component';
-import { RegistrationComponent } from './registration/registration.component';
-import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent,
-    RegistrationComponent,
-    LoginComponent
   ],
   imports: [
     BrowserModule,
